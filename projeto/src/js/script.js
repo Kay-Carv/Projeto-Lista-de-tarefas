5c0465e3120c1ffa06ccb3b93aef26b90e16a0bf
Kayque <kayquecarvalho020@gmail.com>
2025-02-10 11:19:25 -0300

    Criando uma arrow function para validar campo

--- projeto/src/js/script.js
+++ projeto/src/js/script.js
@@ -1,3 +1,18 @@
-//Declarar variáveis
+//Declarar variavel como array
 
 let tarefas =[]; //[] array
+
+//Criar função que valida o preenchimento do campo caso esteja vazio
+
+/*function validarCampo(){
+    let valida = false;
+    if(document.getElementById("task").value == "") valida =true;
+    return valida; 
+}*/
+
+//Arrow function
+const validarCampo=()=>{
+    let valida = false;
+    if(document.getElementById("task").value == "") valida =true;
+    return valida; 
+}
